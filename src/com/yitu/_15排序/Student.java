5c02ef102fe8d18ca0a9e3f5c7c44c18d4b92cf0
yitu <2551434573@qq.com>
2021-07-12 13:46:14 +0800

    新增排序算法稳定性检测

---
+++ src/com/yitu/_15排序/Student.java
@@ -0,0 +1,16 @@
+package com.yitu._15排序;
+
+public class Student implements Comparable<Student>{
+    public int score;
+    public int age;
+
+    public Student(int score, int age) {
+        this.score = score;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return age-o.age;
+    }
+}
