2150179e4efd29b95152c8ff53430c846f2be618
yitu <2551434573@qq.com>
2021-08-18 16:11:32 +0800

    完成算法贪心

---
+++ src/com/yitu/_20贪心/Pirate.java
@@ -0,0 +1,24 @@
+package com.yitu._20贪心;
+
+import java.util.Arrays;
+
+/**
+ * 最优装载问题（加勒比海盗）
+ */
+public class Pirate {
+    public static void main(String[] args) {
+        int[] weights = {3, 5, 4, 10, 7, 14, 2, 11};
+        Arrays.sort(weights);
+        int capacity=30,weight=0,count=0;
+
+        for(int i=0;i<weights.length&&weight<capacity;i++){
+            int newWeight = weight+weights[i];
+            if(newWeight<=capacity){
+                weight=newWeight;
+                count++;
+                System.out.println(weights[i]);
+            }
+        }
+        System.out.println("一共选了"+count+"件古董");
+    }
+}
