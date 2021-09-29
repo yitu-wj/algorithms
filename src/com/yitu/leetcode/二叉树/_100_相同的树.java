b005cfcd8f5ef8c7ae3176ab2899ddf69de1ed5d
yitu <2551434573@qq.com>
2021-09-29 17:13:01 +0800

    leetcode 100相同的树

---
+++ src/com/yitu/leetcode/二叉树/_100_相同的树.java
@@ -0,0 +1,16 @@
+package com.yitu.leetcode.二叉树;
+
+import com.yitu.leetcode.TreeNode;
+
+/**
+ * https://leetcode-cn.com/problems/same-tree/
+ */
+public class _100_相同的树 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q == null) return false;
+        if (p == null && q != null) return false;
+        if (p == null && q == null) return true;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
