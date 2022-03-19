e2c636b00a50eca5659b85af94cd6a3822b81da9
Wang Jie <2551434573@qq.com>
2022-03-19 15:15:15 +0800

    斐波那契数列添加O(N)级别算法

--- src/com/yitu/algorithms/_01时间复杂度/Main.java
+++ src/com/yitu/algorithms/_01时间复杂度/Main.java
@@ -2,10 +2,25 @@ package com.yitu.algorithms._01时间复杂度;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(fib(5));
+        System.out.println(fib1(9));
+        System.out.println(fib2(9));
     }
-    public static int fib(int n) {
-        if (n <= 1) return n;
-        return fib(n - 1) + fib(n - 2);
+
+    public static int fib1(int n) {
+        if (n <= 2) return 1;
+        return fib1(n - 1) + fib1(n - 2);
+    }
+
+    public static int fib2(int n) {
+        if (n <= 2) return 1;
+        int first = 1;
+        int second = 1;
+
+        for (int i = 2; i < n; i++) {
+            int num = first + second;
+            first = second;
+            second = num;
+        }
+        return second;
     }
 }
