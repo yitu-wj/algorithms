199aa1529237b4b63b27f3513c805e0ebd9d4843
Wang Jie <2551434573@qq.com>
2022-03-19 16:24:37 +0800

    删除测试代码

--- src/com/yitu/algorithms/_02动态数组/Main.java
+++ src/com/yitu/algorithms/_02动态数组/Main.java
@@ -1,12 +1,8 @@
 package com.yitu.algorithms._02动态数组;
 
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList=new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            arrayList.add(i);
-        }
-        arrayList.clear();
-        System.out.println(arrayList);
+
     }
 }
