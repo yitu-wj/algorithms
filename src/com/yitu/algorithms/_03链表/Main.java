d381ec056a72ac919456ac688dead2c62ed06508
Wang Jie <2551434573@qq.com>
2022-03-20 16:28:45 +0800

    日常测试代码

--- src/com/yitu/algorithms/_03链表/Main.java
+++ src/com/yitu/algorithms/_03链表/Main.java
@@ -2,15 +2,21 @@ package com.yitu.algorithms._03链表;
 
 
 import com.yitu.algorithms.List;
+import com.yitu.algorithms._02动态数组.ArrayList;
 import com.yitu.algorithms._03链表.circle.DoubleCircleLinkedList;
+import com.yitu.algorithms._03链表.single.HeadLinkedList;
+import com.yitu.algorithms._03链表.single.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        josephus();
-    }
-    static void josephus(){
-        List<Integer> list = new DoubleCircleLinkedList<>();
+        List<Integer> list=new HeadLinkedList<>();
         list.add(1);
         list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(null);
+        System.out.println(list);
+        System.out.println(list.indexOf(null));
     }
 }
