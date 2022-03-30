599b7e7cba571a37c67fa2555608d7fe1747651b
Wang Jie <2551434573@qq.com>
2022-03-30 09:31:36 +0800

    修复二叉树无法打印节点具体内容BUG

--- src/com/yitu/algorithms/BinaryTree.java
+++ src/com/yitu/algorithms/BinaryTree.java
@@ -236,7 +236,7 @@ public class BinaryTree<E> implements BinaryTreeInfo {
 
     @Override
     public Object string(Object node) {
-        return node;
+        return ((Node)node).element;
     }
     public static abstract class Visitor<E>{
         boolean stop;
